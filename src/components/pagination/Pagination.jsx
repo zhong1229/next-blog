bb1bc51aa440ba4a51d76facf6c15bf964cc2f1a
“zhong1229” <3052288628@qq.com>
2023-11-21 10:59:39 +0800

    分页器bug修复

--- src/components/pagination/Pagination.jsx
+++ src/components/pagination/Pagination.jsx
@@ -2,24 +2,36 @@
 
 import React from "react";
 import styles from "./pagination.module.css";
-import { useRouter } from "next/navigation";
+import { useRouter, useSearchParams } from "next/navigation";
 
 const Pagination = ({ page, hasPrev, hasNext }) => {
   const router = useRouter();
-
+  const SearchParams = useSearchParams();
   return (
     <div className={styles.container}>
       <button
         className={styles.button}
         disabled={!hasPrev}
-        onClick={() => router.push(`?page=${page - 1}`)}
+        onClick={() =>
+          router.push(
+            `?${
+              SearchParams.get("cat") ? `cat=${SearchParams.get("cat")}` : ""
+            }&page=${page - 1}`
+          )
+        }
       >
         上一页
       </button>
       <button
         disabled={!hasNext}
         className={styles.button}
-        onClick={() => router.push(`?page=${page + 1}`)}
+        onClick={() =>
+          router.push(
+            `?${
+              SearchParams.get("cat") ? `cat=${SearchParams.get("cat")}` : ""
+            }&page=${page + 1}`
+          )
+        }
       >
         下一页
       </button>
